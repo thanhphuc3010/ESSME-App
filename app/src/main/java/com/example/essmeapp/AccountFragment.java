f3f301ee890764ad1889db22b018e1fd53b74ce9
phucpt <phucpt@moolapp.com>
2022-04-21 23:33:56 +0700

    implement login with firebase

--- app/src/main/java/com/example/essmeapp/AccountFragment.java
+++ app/src/main/java/com/example/essmeapp/AccountFragment.java
@@ -1,64 +1,41 @@
 package com.example.essmeapp;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AccountFragment extends Fragment {
+import com.example.essmeapp.codebase.BaseViewBindingFragment;
+import com.example.essmeapp.databinding.FragmentAccountBinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class AccountFragment extends BaseViewBindingFragment<FragmentAccountBinding> {
     public AccountFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(FragmentAccountBinding::inflate);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignOut:
+                ((MainActivity) getActivity()).signOut();
+                break;
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+    public void initializeViews() {
+
+    }
+
+    @Override
+    public void initializeComponents() {
+
+    }
+
+    @Override
+    public void initializeEvents() {
+        binding.btnSignOut.setOnClickListener(this);
+    }
+
+    @Override
+    public void initializeData() {
+
     }
 }
